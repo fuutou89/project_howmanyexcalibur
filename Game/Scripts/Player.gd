35f15cb10e2c489595351590d17e5337db5603d8
DESKTOP-OOERLCT\me <shengdngtan@gmail.com>
2023-01-15 11:35:38 +0800

    Camera Stuff

--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -1,16 +1,28 @@
 extends Area2D
 
 export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+#var screen_size # Size of the game window.
 
 export var hand_rotate_speed = .1
+const ZOOM_RATE: float = 0.5
+var _target_zoom: float = 1.0
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	#screen_size = get_viewport_rect().size
 	add_to_group("player")
+	
+func _physics_process(delta):
+	$Camera2D.zoom = lerp($Camera2D.zoom, _target_zoom * Vector2.ONE, ZOOM_RATE * delta)
+	set_physics_process(not is_equal_approx($Camera2D.zoom.x, _target_zoom))
 
 func _process(delta):
 	$Hand.rotate(hand_rotate_speed)
+	
+	var childCount = $Hand.get_child_count()
+	var new_zoom = clamp(childCount / 10.0, 1, 3)
+	if _target_zoom != new_zoom:
+		_target_zoom = new_zoom
+		set_physics_process(true)
 	
 	if Input.is_action_just_released("switch_hand"):
 		hand_rotate_speed *= -1
@@ -29,8 +41,8 @@ func _process(delta):
 		velocity = velocity.normalized() * speed
 
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	#position.x = clamp(position.x, 0, screen_size.x)
+	#position.y = clamp(position.y, 0, screen_size.y)
 
 
 func _on_Excalibur_area_entered(area):
